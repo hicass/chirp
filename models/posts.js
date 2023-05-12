431fd922bf80c901808ade56fb0450669e9cc3fa
hicass <cassidyjwalters@gmail.com>
2023-05-12 11:08:46 -0700

    Define the Model for Posts and embedded Comments.

---
+++ models/posts.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    content: { 
+        type: String, 
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    userName: String,
+    userAvatar: String,
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
+
+const commentSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    userName: String,
+    userAvatar: String
+});
+
+module.exports = mongoose.model('Post', postSchema);
